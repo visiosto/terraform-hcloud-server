5ae59d05d366cb2fa91721b5a42b1c1369a15270
Antti Kivi <antti@anttikivi.fi>
2025-04-27 22:25:59 +0300

    fix: use the outputs conditionally

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "ipv4_address" {
   description = "The public IPv4 address attached to the server."
-  value       = hcloud_primary_ip.ipv4[0].ip_address
+  value       = var.enable_ipv4 ? hcloud_primary_ip.ipv4[0].ip_address : null
   sensitive   = true
 }
 
 output "ipv6_address" {
   description = "The public IPv6 address attached to the server."
-  value       = hcloud_primary_ip.ipv6[0].ip_address
+  value       = var.enable_ipv6 ? hcloud_primary_ip.ipv6[0].ip_address : null
   sensitive   = true
 }
